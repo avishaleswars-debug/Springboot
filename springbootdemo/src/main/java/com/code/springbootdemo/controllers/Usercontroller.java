ae2ac9619f80a7a4438dcea85f998e939c306936
avishaleswars-debug <avishaleswars@gmail.com>
2025-11-29 12:53:57 +0530

    new commit

--- springbootdemo/src/main/java/com/code/springbootdemo/controllers/Usercontroller.java
+++ springbootdemo/src/main/java/com/code/springbootdemo/controllers/Usercontroller.java
@@ -1,14 +1,24 @@
 package com.code.springbootdemo.controllers;
 
+import java.util.Arrays;
+
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.code.springbootdemo.model.User;
+import java.util.Arrays;
+import java.util.List;
+
 @RestController
 @RequestMapping("/hello")
 public class Usercontroller {
-@GetMapping
-public String getuser() {
-	return "hello";
-}
+//@GetMapping
+//public String getuser() {
+//	return "hello";
+//}
+	@GetMapping
+	public List<User> getuser() {
+	return Arrays.asList(new User(1L,"ajay","ajay@gmail.com"),new User(2L,"vishal","vishal@gmail.com"));
+	}
 }
