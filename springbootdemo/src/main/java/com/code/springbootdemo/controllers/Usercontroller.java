4f0f58345bfe5f5d5d45abeafb8d5a2b215b9f38
avishaleswars-debug <avishaleswars@gmail.com>
2025-11-30 15:05:11 +0530

    find by id

--- springbootdemo/src/main/java/com/code/springbootdemo/controllers/Usercontroller.java
+++ springbootdemo/src/main/java/com/code/springbootdemo/controllers/Usercontroller.java
@@ -1,9 +1,11 @@
 package com.code.springbootdemo.controllers;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -31,5 +33,9 @@ public class Usercontroller {
 	@PostMapping
 	public UserEntity createuser(@RequestBody UserEntity user){
 return repo.save(user); 
+	}
+	@GetMapping("/{id}")
+	public Optional<UserEntity> getUserBody(@PathVariable Long id) {
+	return repo.findById(id);
 	}
 }
