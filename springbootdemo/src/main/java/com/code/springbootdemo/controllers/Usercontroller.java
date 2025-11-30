434c2f24fe1a432ba7ff23c75a8c1224faaac3db
avishaleswars-debug <avishaleswars@gmail.com>
2025-11-30 15:36:25 +0530

    put method

--- springbootdemo/src/main/java/com/code/springbootdemo/controllers/Usercontroller.java
+++ springbootdemo/src/main/java/com/code/springbootdemo/controllers/Usercontroller.java
@@ -4,9 +4,11 @@ import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -38,4 +40,18 @@ return repo.save(user);
 	public Optional<UserEntity> getUserBody(@PathVariable Long id) {
 	return repo.findById(id);
 	}
+	@PutMapping("/{id}")
+	public ResponseEntity<UserEntity> updateUser(
+	        @PathVariable Long id,
+	        @RequestBody UserEntity user) {
+
+	    UserEntity userdata = repo.findById(id)
+	            .orElseThrow(() -> new RuntimeException("User not found"));
+
+	    userdata.setEmail(user.getEmail());
+	    userdata.setName(user.getName());
+
+	    return ResponseEntity.ok(repo.save(userdata));
+	}
 }
+
